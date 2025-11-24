248286a3fef3c4e8ab6bb3af48d6f1f6d55b474d
ytr0nn234 <g00467242@atu.ie>
2025-11-24 10:26:28 +0000

    Added BankAccount class

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,32 @@
+
+
+public class BankAccount {
+    private String accNo;
+    private String name;
+    private double balance;
+
+    public BankAccount(String accNo, String name, double balance) {
+        if(balance <= 0) {
+            throw new IllegalArgumentException("Balance must be greater than 0.");
+        }
+        this.accNo = accNo;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public BankAccount() {
+
+    }
+
+    public String getAccNo() {
+        return accNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
